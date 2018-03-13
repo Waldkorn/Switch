028a8e9090281ca52c9c9a349ee6f6e82dd4987c
Waldkorn <32877557+Waldkorn@users.noreply.github.com>
2018-03-13 19:02:35 +0100

    added some basic layout

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -17,36 +17,52 @@
 </head>
 <body>
 
-	<nav class="navbar bg-dark">
+	<div class="container ml-0 mr-0 pl-0 pr-0" style="max-width: 100%; min-height:100vh;">
 
-		<a class="navbar-brand" href="#"><img alt="Switch logo" src="/images/Switch-logo.png" height="56px"></a>
+		<nav class="navbar navbar-dark bg-dark" style="color:beige;">
 
-		<div class="navbar-nav">
-			<div class="nav-item"><a class="nav-link" href="#">Test</a></div>
-		</div>
+			<a class="navbar-brand" href="#"><img alt="Switch logo" src="/images/Switch-logo.png" height="56px"></a>
 
-	</nav>
+			<div class="navbar float-right">
+				<div class="nav-item active ml-2">Welcome, user</div>
+				<div class="nav-item active ml-2">Logout</div>
+				<button class="navbar-toggler ml-2" type="button">
+			      <span class="navbar-toggler-icon"></span>
+			    </button>
+			</div>
 
-	<video id="vid1" class="video-js" controls preload="auto" width="640" height="264" data-setup='{}'>
-	    <source src="http://10.0.0.61:8080/hls/test.m3u8" type='application/x-mpegURL'>
-	    <p class="vjs-no-js">
-	    	To view this video please enable JavaScript, and consider upgrading to a web browser that
-	    	<a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a>
-	    </p>
-	</video>
+		</nav>
 
-	<p>test</p>
+		<div class="row" style="min-height:1000px;">
+
+			<div class="col-md-2 bg-dark">
+
+			</div>
+
+			<div class="col-md-6 px-0">
+
+				<video id="vid1" class="video-js" controls preload="auto" data-setup='{}' style="max-width: 100%; min-width: 100%;">
+				    <source src="http://10.0.0.61:8080/hls/test.m3u8" type='application/x-mpegURL'>
+				    <p class="vjs-no-js">
+				    	To view this video please enable JavaScript, and consider upgrading to a web browser that
+				    	<a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a>
+				    </p>
+				</video>
+
+			</div>
+
+			<div class="col-md-4 bg-dark">
+
+			</div>
+
+		</div> <!-- row -->
+	</div> <!-- container -->
 
 	<script src="http://vjs.zencdn.net/6.6.3/video.js"></script>
 	<script src="https://unpkg.com/videojs-flash@2.0.0/dist/videojs-flash.min.js"></script>
 	<script>
 	  videojs.options.flash.swf = 'https://unpkg.com/videojs-swf@5.4.0/dist/video-js.swf';
-	  //videojs('vid1');
 	</script>
-<!-- 	<script>
-	    var vid = document.getElementById("vid1");
-	    var player = videojs(vid, {techOrder: ['flash', 'html5']});
-	</script> -->
 	<script src="{{ asset('js/app.js') }}"></script>
 
 </body>
