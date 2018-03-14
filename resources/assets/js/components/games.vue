3727667b31401fcf150b715ab2fa2832009cb66a
Julia van Drunen <jdrunen@localhost.localdomain>
2018-03-14 16:02:42 +0100

    added vue components for games (frontpage section) and profile sidebad

---
+++ resources/assets/js/components/games.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+              <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="https://seeklogo.com/images/D/dota-2-logo-556BDCC022-seeklogo.com.png" alt="hardcoded example">
+                <div class="card-body">
+                  <h5 class="card-title">currently streaming: [some number]</h5>
+                  <a href="#" class="btn btn-primary">Show list of streamers</a>
+                </div>
+              </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('games list loaded.')
+        }
+    }
+</script>
