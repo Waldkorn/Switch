3b0eb1c42958cbde5a5a472a2393d975825c426b
Julia van Drunen <jdrunen@localhost.localdomain>
2018-03-14 13:28:25 +0100

    now with seeder content

--- database/seeds/GamesTableSeeder.php
+++ database/seeds/GamesTableSeeder.php
@@ -11,10 +11,59 @@ class GamesTableSeeder extends Seeder
      */
     public function run()
     {
+      
       DB::table('games')->insert([
-          'name' => 'minecraft',
-          'description' => 'test',
-          'image_source' => 'link',
-      ]);
+
+                [
+                    'name' => 'Minecraft',
+                    'description' => 'Minecraft is a game about placing blocks and going on adventures.',
+                    'image_source' => 'https://is5-ssl.mzstatic.com/image/thumb/Purple128/v4/f4/0f/0e/f40f0e76-f816-f150-c0ea-55a9a024b3d2/mzl.fmipqnrh.png/246x0w.jpg',
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ],
+                [
+                    'name' => '',
+                    'description' => '',
+                    'image_source' => '',
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ],
+                [
+                    'name' => '',
+                    'description' => '',
+                    'image_source' => '',
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ],
+                [
+                    'name' => '',
+                    'description' => '',
+                    'image_source' => '',
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ],
+                [
+                    'name' => '',
+                    'description' => '',
+                    'image_source' => '',
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ],
+                [
+                    'name' => '',
+                    'description' => '',
+                    'image_source' => '',
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ],
+                [
+                    'name' => '',
+                    'description' => '',
+                    'image_source' => '',
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ]
+
+            ]);
     }
 }
