7f311c20133e728adae9ef13ededc83ae9f5888c
Julia van Drunen <jdrunen@localhost.localdomain>
2018-03-15 15:43:34 +0100

    moved view routes from api.php to web.php

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,8 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/frontpage',"API\FrontpageController@view");
+Route::get('/profilepage', "API\ProfileController@view");
 
 Route::get('/', function () {
     return view('homepage');
