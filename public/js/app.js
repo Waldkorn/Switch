c8bf23ae8e3531b63eb5a0e68c885d579d4774b4
Julia van Drunen <jdrunen@localhost.localdomain>
2018-03-23 09:44:43 +0100

    merge stuff

--- public/js/app.js
+++ public/js/app.js
@@ -110209,6 +110209,28 @@ module.exports = Component.exports
 //
 //
 //
+<<<<<<< HEAD
+=======
+//
+//
+//
+//
+
+/* harmony default export */ __webpack_exports__["default"] = ({
+  data: function data() {
+    return {
+      games: null
+    };
+  },
+  mounted: function mounted() {
+    var _this = this;
+
+    axios.get('/api/games').then(function (response) {
+      _this.games = response.data;
+    });
+  }
+});
+>>>>>>> merge stuff
 
 /***/ }),
 /* 167 */
@@ -112365,7 +112387,6 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
     uploadimage: function uploadimage() {
       var formData = new FormData();
       formData.append("profileimage", document.getElementById('profileimage').files[0]);
-      console.log(formData);
       axios.post('/api/uploadimage', formData, { headers: { 'Content-Type': 'multipart/form-data' } }).then(function (response) {
         console.log('file sent');
       });
@@ -112443,7 +112464,16 @@ var staticRenderFns = [
                 },
                 [
                   _c("div", { staticClass: "card-body" }, [
-                    _c("form", [
+                    _c("form", { attrs: { enctype: "multipart/form-data" } }, [
+                      _c("input", {
+                        attrs: {
+                          type: "hidden",
+                          name: "_token",
+                          id: "csrf-token"
+                        },
+                        domProps: { value: _vm.csrftoken }
+                      }),
+                      _vm._v(" "),
                       _vm._m(2),
                       _vm._v(" "),
                       _c(
@@ -112568,7 +112598,7 @@ var staticRenderFns = [
     var _c = _vm._self._c || _h
     return _c("div", { staticClass: "form-group" }, [
       _c("label", { attrs: { for: "profileimage" } }, [
-        _vm._v("Choose your profile picture:")
+        _vm._v("Example file input")
       ]),
       _vm._v(" "),
       _c("input", {
