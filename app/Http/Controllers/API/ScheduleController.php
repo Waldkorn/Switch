aae032cfc78fcfbe700ccf77b7c292db1feef058
Julia van Drunen <jdrunen@localhost.localdomain>
2018-03-26 16:03:29 +0200

    edited schedulemigration, created schedule controller and start of create function

---
+++ app/Http/Controllers/API/ScheduleController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
+use App\User;
+use App\Game;
+use App\Profilecontent;
+use App\Schedule;
+
+class ScheduleController extends Controller
+{
+    public function create(){
+      $type = request('type');
+      $user = Auth::user();
+      $eventname = request('name');
+      switch ($type) {
+    case "once":
+        $start = request('start');
+        $stop = request('stop');
+        $event = Schedule::create([
+            'title' => request('title'),
+            'user_id' => $user->id,
+            'streamer_name' => $user->name,
+            'game_id' => request('game_id'),
+            'start' => request('start'),
+            'stop' => request('stop'),
+            'type' => $type,
+            'tag' => request('tag'),
+          ]);
+
+        break;
+    case "daily":
+        echo "Your favorite color is blue!";
+        break;
+    case "weekly":
+        echo "Your favorite color is green!";
+        break;
+    case "monthly":
+        echo "Your favorite color is green!";
+        break;
+}
+    }
+}
