b3dcc2f6c8e530958eccdffb111a62395abe5b0c
Julia van Drunen <jdrunen@localhost.localdomain>
2018-03-26 15:35:41 +0200

    stream table dummy css

--- database/migrations/2018_03_26_122916_create_schedules_table.php
+++ database/migrations/2018_03_26_122916_create_schedules_table.php
@@ -16,6 +16,7 @@ class CreateSchedulesTable extends Migration
       Schema::create('schedules', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('user_id')->unsigned();
+            $table->string('streamername');
             $table->string('name');
             $table->dateTime('start');
             $table->dateTime('stop');
