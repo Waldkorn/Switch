d96b814c485af833ed183bac2f21c1be2c0db175
Julia van Drunen <jdrunen@localhost.localdomain>
2018-03-27 15:00:29 +0200

    carbon now parses datetime-local

--- routes/api.php
+++ routes/api.php
@@ -64,6 +64,7 @@ Route::post('/streamkey', 'API\DashboardController@streamkey');
 Route::post('/updateabout', 'API\DashboardController@updateAbout')->middleware('auth:api');
 Route::post('/stream', 'API\StreamController@golive');
 Route::get('/currentdate','API\ScheduleController@currentdate');
+Route::post('/addschedule','API\ScheduleController@create');
 
 Route::get('/profilecontent', 'API\DashboardController@getcontent')->middleware('auth:api');
 
