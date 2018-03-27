cede3a4fd8278db6587ec68259a5bee0987a758b
Julia van Drunen <jdrunen@localhost.localdomain>
2018-03-27 10:13:23 +0200

    added carbon/carbon to schedulecontroller

--- app/Http/Controllers/API/ScheduleController.php
+++ app/Http/Controllers/API/ScheduleController.php
@@ -6,7 +6,6 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
-
 use Carbon\Carbon;
 use App\User;
 use App\Game;
