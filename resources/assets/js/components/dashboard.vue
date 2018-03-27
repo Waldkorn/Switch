dd9e6f70b6ccc8c0b0a7ddf4bf24ca7c2c69e1cb
Julia van Drunen <jdrunen@localhost.localdomain>
2018-03-27 11:36:17 +0200

    added npm vue date picker and moment, reinstalled npm again, added date+time picker to dashboard.vue

--- resources/assets/js/components/dashboard.vue
+++ resources/assets/js/components/dashboard.vue
@@ -263,6 +263,7 @@
                   <input type="text" class="form-control" id="single_title" name="single_title" placeholder="Event Name">
                 </div>
                 <div class="form-row">
+<<<<<<< 84547d4b1d31833de7630e11f624809d7c42d213
                   <div class="col">
                       <span>Start：</span>
                       <input type="datetime-local" name="single_start" id="single_start" :min="currentdate">
@@ -270,6 +271,23 @@
                 <div class="col">
                       <span>End：</span>
                       <input type="datetime-local" name="single_end" id="single_end" :min="currentdate">
+=======
+                <div class="col">
+                  <div class="form-group">
+                    <div class="row">
+                      <span>Departure Date：</span>
+                      <date-picker :date="startTime" :option="option" :limit="limit"></date-picker>
+                    </div>
+                  </div>
+                </div>
+                <div class="col">
+                  <div class="form-group">
+                    <div class="row">
+                      <span>Departure Date：</span>
+                      <date-picker :date="startTime" :option="option" :limit="limit"></date-picker>
+                    </div>
+             </div>
+>>>>>>> added npm vue date picker and moment, reinstalled npm again, added date+time picker to dashboard.vue
                 </div>
                 </div>
                 <div class="form-group">
@@ -389,6 +407,62 @@ export default {
   				profiledash: false,
   				channeldash: false,
           scheduledash: false,
+
+          startTime: {
+                time: ''
+              },
+              endtime: {
+                time: ''
+              },
+
+              option: {
+                type: 'day',
+                week: ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'],
+                month: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+                format: 'YYYY-MM-DD',
+                placeholder: 'when?',
+                inputStyle: {
+                  'display': 'inline-block',
+                  'padding': '6px',
+                  'line-height': '22px',
+                  'font-size': '16px',
+                  'border': '2px solid #fff',
+                  'box-shadow': '0 1px 3px 0 rgba(0, 0, 0, 0.2)',
+                  'border-radius': '2px',
+                  'color': '#5F5F5F'
+                },
+                color: {
+                  header: '#ccc',
+                  headerText: '#f00'
+                },
+                buttons: {
+                  ok: 'Ok',
+                  cancel: 'Cancel'
+                },
+                overlayOpacity: 0.5, // 0.5 as default
+                dismissible: true // as true as default
+              },
+              timeoption: {
+                type: 'min',
+                week: ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'],
+                month: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+                format: 'YYYY-MM-DD HH:mm'
+              },
+              multiOption: {
+                type: 'multi-day',
+                week: ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'],
+                month: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+                format:"YYYY-MM-DD HH:mm"
+              },
+              limit: [{
+                type: 'weekday',
+                available: [1, 2, 3, 4, 5]
+              },
+              {
+                type: 'fromto',
+                from: '2016-02-01',
+                to: '2016-02-20'
+              }]
       }
     },
 
