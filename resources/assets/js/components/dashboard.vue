c98b1983263a3ead6b32448a1c578d8e26056f4c
Julia van Drunen <jdrunen@localhost.localdomain>
2018-03-27 11:36:17 +0200

    added npm vue date picker and moment, reinstalled npm again, added date+time picker to dashboard.vue

--- resources/assets/js/components/dashboard.vue
+++ resources/assets/js/components/dashboard.vue
@@ -235,14 +235,18 @@
                 <div class="form-row">
                 <div class="col">
                   <div class="form-group">
-                 <label for="schedulestart">Start:</label>
-                 <input type="datetime-local" id="schedulestart" name="bdaytime">
-               </div>
+                    <div class="row">
+                      <span>Departure Date：</span>
+                      <date-picker :date="startTime" :option="option" :limit="limit"></date-picker>
+                    </div>
+                  </div>
                 </div>
                 <div class="col">
                   <div class="form-group">
-               <label for="scheduleend">End:</label>
-                <input type="datetime-local" id="scheduleend" name="bdaytime">
+                    <div class="row">
+                      <span>Departure Date：</span>
+                      <date-picker :date="startTime" :option="option" :limit="limit"></date-picker>
+                    </div>
              </div>
                 </div>
               </div>
@@ -279,8 +283,9 @@
 
 <script>
 
-export default {
+import myDatepicker from 'vue-datepicker'
 
+export default {
 
     data:function(){
       return{
@@ -291,7 +296,67 @@ export default {
   				profiledash: false,
   				channeldash: false,
           scheduledash: false,
+
+          startTime: {
+                time: ''
+              },
+              endtime: {
+                time: ''
+              },
+
+              option: {
+                type: 'day',
+                week: ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'],
+                month: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+                format: 'YYYY-MM-DD',
+                placeholder: 'when?',
+                inputStyle: {
+                  'display': 'inline-block',
+                  'padding': '6px',
+                  'line-height': '22px',
+                  'font-size': '16px',
+                  'border': '2px solid #fff',
+                  'box-shadow': '0 1px 3px 0 rgba(0, 0, 0, 0.2)',
+                  'border-radius': '2px',
+                  'color': '#5F5F5F'
+                },
+                color: {
+                  header: '#ccc',
+                  headerText: '#f00'
+                },
+                buttons: {
+                  ok: 'Ok',
+                  cancel: 'Cancel'
+                },
+                overlayOpacity: 0.5, // 0.5 as default
+                dismissible: true // as true as default
+              },
+              timeoption: {
+                type: 'min',
+                week: ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'],
+                month: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+                format: 'YYYY-MM-DD HH:mm'
+              },
+              multiOption: {
+                type: 'multi-day',
+                week: ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'],
+                month: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+                format:"YYYY-MM-DD HH:mm"
+              },
+              limit: [{
+                type: 'weekday',
+                available: [1, 2, 3, 4, 5]
+              },
+              {
+                type: 'fromto',
+                from: '2016-02-01',
+                to: '2016-02-20'
+              }]
       }
+    },
+
+    components: {
+    'date-picker': myDatepicker
     },
     props: ['user'],
     mounted() {
@@ -385,4 +450,5 @@ export default {
 >>>>>>> schedule migration, model, and form in dashboard.vue
     }
   }
+
 </script>
