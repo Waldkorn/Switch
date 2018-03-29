80f87cf34c8bd75c1f608b373db6ddf0b28f0456
Julia van Drunen <jdrunen@localhost.localdomain>
2018-03-29 16:01:28 +0200

    schedule save works, get request also works and sorts by starting date

--- app/Schedule.php
+++ app/Schedule.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Schedule extends Model
 {
   protected $fillable = [
-    'title', 'game_id', 'user_id', 'tag', 'start_time','end_time','start_date','end_date', 'streamer_name', 'type'
+    'title', 'game_id', 'user_id', 'tag', 'start_time','end_time','start_date','end_date', 'streamer_name', 'type','day',
   ];
 
     public function user()
