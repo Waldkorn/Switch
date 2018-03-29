1ab5c613e6fc8e7380d50674bd96730ffe16f373
Waldkorn <32877557+Waldkorn@users.noreply.github.com>
2018-03-29 15:53:27 +0200

    minor fixes

--- resources/assets/js/components/games.vue
+++ resources/assets/js/components/games.vue
@@ -1,8 +1,8 @@
 <template>
-  <div id="games-main" class="col-md-5 d-flex flex-wrap justify-content-end" style="padding-right:0;max-height: 1px;">
+  <div id="games-main" class="col-md-5 d-flex flex-wrap justify-content-end" style="padding-right:0;max-height: 1px;float:right;">
     <div class="p-1" style="padding:0;margin:0;float:right" v-for="game in games">
       <a v-bind:href="'/game/' + game.name">
-        <div class="card" style="width: 175px;height:100%;">
+        <div class="card" style="width:175px;height:100%;">
           <img class="card-img-top" :src="game.image_source" :alt="game.name" height="150" width="150">
 
           <div class="card-body">
