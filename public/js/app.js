37535d6070db944f5e9cd6f24b53faf7a60c04a0
Waldkorn <32877557+Waldkorn@users.noreply.github.com>
2018-03-30 11:02:08 +0200

    fixed bug where the nowlivebar wouldn't load on frontpage

--- public/js/app.js
+++ public/js/app.js
@@ -121639,7 +121639,6 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
   mounted: function mounted() {
     var _this = this;
 
-    console.log(this.user);
     if (this.user != undefined) {
       axios.get('/api/listusers').then(function (response) {
         _this.users = response.data;
