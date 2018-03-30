bac788bf0dffc15d25d316c47f0af284906672c6
Waldkorn <32877557+Waldkorn@users.noreply.github.com>
2018-03-30 10:37:45 +0200

    a few emotes are now available in chat

--- database/seeds/userFactory.php
+++ database/seeds/userFactory.php
@@ -20,16 +20,6 @@ class userFactory extends Seeder
 
     	foreach(App\User::all() as $user)
     	{
-
-        // $user->stream()->save(factory(App\Stream::class)->make());
-    		// DB::table('streams')->insert([
-    		// 	'user_id' => $user->id,
-    		// 	'game_id' => random_int(1,7),
-    		// 	'title' => $faker->sentence($nbWords = 6),
-      //     'created_at' => date('Y-m-d H:i:s'),
-      //     'updated_at' => date('Y-m-d H:i:s'),
-    		// ]);
-
         DB::table('profilecontents')->insert([
     			'user_id' => $user->id,
     			'about' => 'Hello I am a bot!',
@@ -37,18 +27,17 @@ class userFactory extends Seeder
           'created_at' => date('Y-m-d H:i:s'),
           'updated_at' => date('Y-m-d H:i:s'),
     		]);
-
     	}
 
 	    foreach(App\User::all() as $user)
-        {
-        	for ($i = 0 ; $i < 5 ; $i++)
-        	{
-	            DB::table('followers')->insert([
-	            	'follower_id' => $user->id,
-	            	'streamer_id' => random_int(1, 50)
-	            ]);
-	   		  }
-        }
+      {
+      	for ($i = 0 ; $i < 5 ; $i++)
+      	{
+            DB::table('followers')->insert([
+            	'follower_id' => $user->id,
+            	'streamer_id' => random_int(1, 50)
+            ]);
+   		  }
+      }
     }
 }
