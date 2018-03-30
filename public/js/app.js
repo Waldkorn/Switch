8b5a7386b1e6f766818107de67c2ab67ec853b72
Julia van Drunen <jdrunen@localhost.localdomain>
2018-03-30 10:53:45 +0200

    removed edit and delete buttons from schedule on profile page

--- public/js/app.js
+++ public/js/app.js
@@ -119816,8 +119816,6 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 //
 //
 //
-//
-//
 
 /* harmony default export */ __webpack_exports__["default"] = ({
   props: ['profile'],
@@ -119886,10 +119884,6 @@ var render = function() {
             _vm._v(" "),
             _c("td", [_vm._v(_vm._s(schedule.tag))]),
             _vm._v(" "),
-            _vm._m(1, true),
-            _vm._v(" "),
-            _vm._m(2, true),
-            _vm._v(" "),
             _c("td")
           ])
         })
@@ -119916,22 +119910,6 @@ var staticRenderFns = [
         _vm._v(" "),
         _c("th", { attrs: { scope: "col" } }, [_vm._v("tag")])
       ])
-    ])
-  },
-  function() {
-    var _vm = this
-    var _h = _vm.$createElement
-    var _c = _vm._self._c || _h
-    return _c("td", [
-      _c("div", { staticClass: "btn btn-info" }, [_vm._v("edit")])
-    ])
-  },
-  function() {
-    var _vm = this
-    var _h = _vm.$createElement
-    var _c = _vm._self._c || _h
-    return _c("td", [
-      _c("div", { staticClass: "btn btn-danger" }, [_vm._v("delete")])
     ])
   }
 ]
