0181403e8f81ac69e04e7651bc53bb94dce8f9ac
Julia van Drunen <jdrunen@localhost.localdomain>
2018-03-30 17:59:18 +0200

    merge fix

--- app/User.php
+++ app/User.php
@@ -76,7 +76,7 @@ class User extends Authenticatable
     {
         return $this->hasMany(Announcement::class);
     }
-
+    
     public function schedule()
     {
         return $this->hasOne(Schedule::class, 'user_id', 'id');
