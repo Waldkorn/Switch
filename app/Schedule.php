28f598f478314818d76797ab69a6bbbcd037608c
Julia van Drunen <jdrunen@localhost.localdomain>
2018-03-30 16:07:45 +0200

    I forgot to save something, no idea what

--- app/Schedule.php
+++ app/Schedule.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Schedule extends Model
 {
-
+  
   protected $fillable = [
     'title', 'game_id', 'user_id', 'tag', 'start_time','end_time','start_date','end_date', 'streamer_name', 'type','day',
   ];
