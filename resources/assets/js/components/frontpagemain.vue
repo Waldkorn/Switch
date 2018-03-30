2b7eda27153da15500b11c2e28593a7cf0bdb7ad
Waldkorn <32877557+Waldkorn@users.noreply.github.com>
2018-03-30 17:18:15 +0200

    merging

--- resources/assets/js/components/frontpagemain.vue
+++ resources/assets/js/components/frontpagemain.vue
@@ -4,7 +4,7 @@
     <div class="card mb-2" v-for="announcement in announcements">
     	<div class="card-body bg-dark" style="color:rgb(245, 245, 220);">
     		<div class="card-title">
-    			<img height="32px" :src=announcement.user.profilecontent.img_url><span><a :href="'/profilepage/' + announcement.user.name"><h3 class="ml-2" style="display:inline">{{announcement.user.name}}</h3></a><br><h5 class="pt-1">{{ announcement.title }}</h5></span>
+    			<img height="32px" :src=announcement.user.profilecontent.img_url><span><a :href="'/profilepage/' + announcement.user.name" style="color:#dc3545;"><h3 class="ml-2" style="display:inline">{{announcement.user.name}}</h3></a><br><h5 class="pt-1">{{ announcement.title }}</h5></span>
     		</div>
     		<hr>
     		<div class="card-text">
