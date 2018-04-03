4607c4e9c0adfa8d2a88a748eeff4f2651581237
Julia van Drunen <jdrunen@localhost.localdomain>
2018-04-03 12:05:28 +0200

    upcoming schedule vue component. copied upcomingschedules function to frontpage controller and api.php

--- app/Http/Controllers/API/FrontpageController.php
+++ app/Http/Controllers/API/FrontpageController.php
@@ -62,4 +62,48 @@ class FrontpageController extends Controller
 
   }
 
+  //shows events from followed users that start within the next hour
+  public function upcomingstreams(){
+    $user = Auth::user();
+    $followings = $user->followings()->get();
+    $followings_ids = $followings->pluck('id');
+    $games = Game::get();
+
+    $streams= Schedule::
+                whereIn('user_id', $followings_ids)
+                ->where(function ($query)
+                        {
+                            $query->where('start_date', '>=', Carbon::now('Europe/Amsterdam'))
+                                  ->where('start_date', '<=', Carbon::now('Europe/Amsterdam')->addHours(1));
+                        })
+                ->orWhere(function ($query)
+                        {
+                            $query->where('type','daily')
+                                  ->Where('start_time', '>=', Carbon::now('Europe/Amsterdam')->toTimeString())
+                                  ->Where('start_time', '<=', Carbon::now('Europe/Amsterdam')->addHours(1)->toTimeString());
+                        })
+                ->orWhere(function ($query)
+                      {
+                            $query->where('day', '=', Carbon::now('Europe/Amsterdam')->format('l'))
+                                  ->Where('start_time', '>=', Carbon::now('Europe/Amsterdam')->toTimeString())
+                                  ->Where('start_time', '<=', Carbon::now('Europe/Amsterdam')->addHours(1)->toTimeString());
+                      })
+                ->get();
+    //adds streamer name and start/end date to objects
+    foreach($streams as $stream){
+      $streamer_name = $followings->firstWhere('id', $stream->user_id)->name;
+      $stream->streamer = $streamer_name;
+        if($stream->type != 'single'){
+          $today = Carbon::now('Europe/Amsterdam')->toDateString();
+          $start = $today." ".$stream->start_time;
+          $end = $today." ".$stream->end_time;
+          $stream->start_date = $start;
+          $stream->end_date = $end;
+        }
+    };
+
+    return $streams;
+
+  }
+
 }
